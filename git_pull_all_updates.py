ee486612e63a5a8ecf27339558ff34d4846b56a4
WangBin <13324558170@163.com>
2017-11-25 16:57:14 +0800

    filter self python file
    ok

--- git_pull_all_updates.py
+++ git_pull_all_updates.py
@@ -1,5 +1,6 @@
 import os
 import os.path
+import sys
 import subprocess
 
 git_cmd = "git -C {0} pull origin master"
@@ -8,5 +9,8 @@ parent_dir_abspath = os.path.abspath(".")
 subdir_names = os.listdir(parent_dir_abspath)
 
 for subdir_name in subdir_names:
+	if sys.argv[0] == subdir_name:
+		continue
+
 	workdir_path = os.path.join(parent_dir_abspath, subdir_name)
 	subprocess.run(["cmd.exe", "/C", git_cmd.format(workdir_path)])
