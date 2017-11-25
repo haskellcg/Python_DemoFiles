ad585fd9f3aa4b274d58b91712b7e7d1abdaa2fb
WangBin <13324558170@163.com>
2017-11-25 16:44:08 +0800

    initial content
    ok

--- git_pull_all_updates.py
+++ git_pull_all_updates.py
@@ -1,1 +1,12 @@
+import os
+import os.path
+import subprocess
 
+git_cmd = "git -C {0} pull origin master"
+
+parent_dir_abspath = os.path.abspath("..")
+subdir_names = os.listdir(parent_dir_abspath)
+
+for subdir_name in subdir_names:
+	workdir_path = os.path.join(parent_dir_abspath, subdir_name)
+	subprocess.run(["cmd.exe", "/C", git_cmd.format(workdir_path)])
