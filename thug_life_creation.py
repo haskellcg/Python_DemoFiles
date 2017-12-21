e1e83f274ab148ef402ed4514ed83d37eebd3b98
haskellcg <13324558170@163.com>
2017-12-21 12:49:52 +0800

    add part of code and test image

--- thug_life_creation.py
+++ thug_life_creation.py
@@ -1,1 +1,40 @@
+'''
+  Information:
+  * Date: 2017-12-21
+  * Brief: add thug_life tag for image
+  * Author: haskellcg
 
+  Platform: 
+  * Windows7_x64
+  * Python3.6_x64
+	  
+  Libs:
+  * dlib (pip install dlib-19.8.1-cp36-cp36m-win_amd64.whl)
+    * numpy
+  * moviepy (pip install moviepy)
+    * decorator
+	* imageio
+	* olefile
+	* pillow
+	* tqdm
+  * imutils (pip install imutils)
+  * cv2 (pip install opencv-python)
+	
+  Problems:
+  * NeedDownloadError('Need ffmpeg exe. 'imageio.core.fetching.NeedDownloadError: Need ffmpeg exe. You can download it by calling: imageio.plugins.ffmpeg.download() => 按照报错操作即可
+  
+'''
+
+import dlib
+import argparse
+import numpy as np
+import moviepy.editor as mpy
+
+from PIL import Image
+from imutils import face_utils
+
+src_image_file_path = "./resources/thug_life_creation_1.jpg"
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-image", required = false, help = "path to input image")
+args = parser.parse_args()
