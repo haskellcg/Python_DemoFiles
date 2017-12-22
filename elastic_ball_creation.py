62bb2faa9460485cd1efd7920a7edc71d70a1a68
haskellcg <13324558170@163.com>
2017-12-22 10:19:06 +0800

    add elastic_ball gif

---
+++ elastic_ball_creation.py
@@ -0,0 +1,72 @@
+'''
+  Information:
+  * Date: 2017-12-21
+  * Brief: create japanese flag gif with python
+  * Author: haskellcg
+
+  Platform: 
+  * Windows7_x64
+  * Python3.6_x64
+      
+  Libs:
+  * moviepy (pip install moviepy)
+    * decorator
+    * imageio
+    * olefile
+    * pillow
+    * tqdm
+  * gizeh (VC++ 14.0: http://landinghub.visualstudio.com/visual-cpp-build-tools)
+    * cairocffi
+    * cffi
+    * pycparser
+    
+  Problems:
+  * OSError: dlopen() failed to load a library: cairo / cairo-2 => 下载GTK+，并
+    将bin目录加入环境变量(http://win32builder.gnome.org/)
+  
+'''
+import numpy as np
+import gizeh as gz
+import moviepy.editor as mpy
+
+W, H = 200, 75
+D = 3
+r = 10
+DJ, HJ = 50, 35
+ground = 0.75 * H
+outputdir = "./output/"
+
+gradient = gz.ColorGradient(type = "radial",
+							stops_colors = [(0, (1, 0, 0)), (1, (0.1, 0, 0))],
+							xy1 = [0.3, -0.3],
+							xy2 = [0, 0],
+							xy3 = [0, 1.4])
+
+def make_frame(t):
+	surface = gz.Surface(W, H, bg_color = (1, 1, 1))
+	x = (-W / 3) + (5 * W / 3) * (t / D)
+	y = ground - HJ * 4 * (x % DJ) * (DJ - (x % DJ)) / DJ ** 2
+	coef = (HJ - y) / HJ
+	shadow_gradient = gz.ColorGradient(type = "radial",
+									   stops_colors = [(0, (0, 0, 0, 0.2 - coef / 5)),
+									   				  (1, (0, 0, 0, 0))],
+									   xy1 = [0, 0],
+									   xy2 = [0, 0],
+									   xy3 = [0, 1.4])
+	shadow = gz.circle(r = (1 - coef / 4),
+					   fill = shadow_gradient
+					  ).scale(r, r / 2
+					  ).translate((x, ground + r / 2))
+	shadow.draw(surface)
+	ball = gz.circle(r = 1,
+					 fill = gradient
+					).scale(r
+					).translate((x, y))
+	ball.draw(surface)
+	return surface.get_npimage()
+
+
+clip = mpy.VideoClip(make_frame, duration = D)
+clip.write_gif(outputdir + "elastic_ball_creation.gif", 
+			   fps = 25, 
+			   opt = "OptimizePlus")
