a69e8ebf877a254687549bbc8bdb91bcc0bb3cc0
haskellcg <13324558170@163.com>
2017-12-26 20:03:24 +0800

    init file collective_intelligence_discovering_groups

---
+++ collective_intelligence_discovering_groups.py
@@ -0,0 +1,16 @@
+"""
+    Information:
+    * Date: 2017-12-26
+    * Brief: making tests for programming collective intelligence chapter 3
+             discovering groups
+    * Author: haskellcg
+
+    Platform:
+    * Windows7_X64
+    * Python3.6_X64
+
+    Libs:
+
+    Problems:
+
+"""
