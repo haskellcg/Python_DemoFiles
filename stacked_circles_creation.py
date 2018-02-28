ea89df0e8ca6ed55578aafb13a2e4d176cb16961
haskellcg <13324558170@163.com>
2018-02-28 13:46:53 +0800

    change color

--- stacked_circles_creation.py
+++ stacked_circles_creation.py
@@ -30,7 +30,7 @@ import numpy as np
 import gizeh as gz
 import moviepy.editor as mpy
 
-W, H = 256, 256
+W, H = 400, 400
 DURATION = 2.0
 NDISKS_PER_CYCLE = 8
 SPEED = 0.05
@@ -46,7 +46,7 @@ def make_frame(t):
         a = (np.pi / NDISKS_PER_CYCLE) * (N - i - 1)
         r = np.maximum(0, 0.05 * (t + t0 - dt * (N - i - 1)))
         center = W * (0.5 + gz.polar2cart(r, a))
-        color = 3 * ((1.0 * i / NDISKS_PER_CYCLE) % 1.0, )
+        color = 1 * ((1.0 * i / NDISKS_PER_CYCLE) % 1.0, 1, 0)
         circle = gz.circle(r = 0.3 * W,
                            xy = center,
                            fill = color,
